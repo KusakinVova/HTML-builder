6881b090f389fce1e9b23b91430e3a6aad6223dd
Vladimir <kusakinvova@gmail.com>
2021-11-09 03:04:01 +0400

    feat: add 04-write-file  (Mon, Nov 08, 2021 23:04:01 +0000)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fsPromises = require('fs').promises;
+const path = require('path');
 
+const src = path.join(__dirname, '/files');
+const dest = path.join(__dirname, '/files-copy');
+
+async function copyDir(src,dest) {
+  const entries = await fsPromises.readdir(src, {withFileTypes: true});
+  await fsPromises.rm(dest, { recursive: true, force: true });
+  await fsPromises.mkdir(dest);
+  for(let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    if(entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fsPromises.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+copyDir(src,dest);
