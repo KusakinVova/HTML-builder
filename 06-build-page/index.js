f48177f74a38caa3845615326ba21c0f23ffc549
Vladimir <kusakinvova@gmail.com>
2021-11-09 16:52:35 +0400

    feat: 06-task change url dir  (Tue, Nov 09, 2021 12:52:35 +0000)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -46,14 +46,14 @@ async function buildLayout(nameLayout, sourceTemplate, sourceComponents){
 
 
 async function buildProject(dirProject, nameLayout, nameStyle, nameAssetsDir){
-  if(dirProject == undefined) dirProject = path.join(__dirname, '/project-dist');
+  if(dirProject == undefined) dirProject = path.join(__dirname, 'project-dist');
   if(nameLayout == undefined) nameLayout = path.join( dirProject, 'index.html');
   if(nameStyle == undefined) nameStyle = path.join(dirProject, 'style.css');
-  if(nameAssetsDir == undefined) nameAssetsDir = path.join(dirProject, '/assets');
+  if(nameAssetsDir == undefined) nameAssetsDir = path.join(dirProject, 'assets');
   
-  const dirAssetsSource = path.join(__dirname, '/assets');
-  const dirStylesSource = path.join(__dirname, '/styles');
-  const sourceComponents = path.join(__dirname, '/components');
+  const dirAssetsSource = path.join(__dirname, 'assets');
+  const dirStylesSource = path.join(__dirname, 'styles');
+  const sourceComponents = path.join(__dirname, 'components');
   const sourceTemplate = path.join(__dirname, 'template.html');
 
   await fsPromises.rm(dirProject, { recursive: true, force: true });
